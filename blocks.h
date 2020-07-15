6e9f45e6a7e7146a74d21279760999142d88542a
Noah Swerhun <nswerhun@gmail.com>
2020-07-15 17:16:56 -0500

    added weather block

--- blocks.h
+++ blocks.h
@@ -1,9 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"", "statusbat",  10,   0},
-    {"📄 ", "statusmem",  5,   0},
+    {"", "statusweather",  5,   0},
+    {"📃 ", "statusmem",  5,   0},
     {"🌡 ", "statuscpu",  5,   0},
+    {"", "statusbat",  10,   0},
     {"🗓 ", "statusdate",  1,   0},
     {"⌚ ", "statustime",  1,   0},
 };
