01edd2eecccde06f279447e82eab9c67fbebe97e
Noah Swerhun <nswerhun@gmail.com>
2020-07-15 16:26:27 -0500

    added

--- blocks.h
+++ blocks.h
@@ -1,6 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+    {"", "statusbat",  10,   0},
     {"📄 ", "statusmem",  5,   0},
     {"🌡 ", "statuscpu",  5,   0},
     {"🗓 ", "statusdate",  1,   0},
